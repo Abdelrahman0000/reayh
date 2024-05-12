068c0047ab289034868a5fb8c20fb0991bf571b0
Abdelrahman0000 <khlyftbdh5@gmail.com>
2024-05-12 17:24:58 +0200

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+///////////////nav
+
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".small-nav").toggleClass("show");
+  });
+});
+
 ///////////////chane lang
 
 // Select the language toggle link
@@ -19,64 +27,55 @@ langToggle.addEventListener("click", function (event) {
   }
 });
 
-///////////////nav
-
-$(document).ready(function () {
-  $(".navbar-toggler").click(function () {
-    $(".small-nav").toggleClass("show");
-  });
-});
-
 ////////////////modal
-// احصل على كل علامة تبويب
-var tabs = document.querySelectorAll("#tabContainer .navLink");
+// Function to hide the first modal and show the second modal
+function hideFirstModalAndShowSecondModal() {
+  var firstModal = document.getElementById("firstModal");
+  var secondModal = document.getElementById("secondModal");
 
-// اضبط event listener لكل علامة تبويب
-tabs.forEach(function (tab) {
-  tab.addEventListener("click", function () {
-    // اغلق جميع المودالات المفتوحة
-    var openModals = document.querySelectorAll(".modal.show");
-    openModals.forEach(function (modal) {
-      var modalInstance = bootstrap.Modal.getInstance(modal);
-      if (modalInstance) {
-        modalInstance.hide();
-      }
-    });
+  // Remove "show" class and add display: none inline style to hide the first modal
+  firstModal.classList.remove("show");
+  firstModal.style.display = "none";
 
-    // افتح المودال المناسب بناءً على علامة التبويب
-    if (tab.getAttribute("href") === "#firstTab") {
-      var firstModal = new bootstrap.Modal(
-        document.getElementById("firstModal")
-      );
-      firstModal.show();
-    } else if (tab.getAttribute("href") === "#secondTab") {
-      var secondModal = new bootstrap.Modal(
-        document.getElementById("secondModal")
-      );
-      secondModal.show();
-    } else if (tab.getAttribute("href") === "#thirdTab") {
-      var thirdModal = new bootstrap.Modal(
-        document.getElementById("thirdModal")
-      );
-      thirdModal.show();
-    }
-  });
-});
-//////////////////////////////
-document.querySelector(".join-us").addEventListener("click", function () {
-  // اغلق جميع المودالات المفتوحة
-  var openModals = document.querySelectorAll(".modal.show");
-  openModals.forEach(function (modal) {
-    var modalInstance = bootstrap.Modal.getInstance(modal);
-    if (modalInstance) {
-      modalInstance.hide();
-    }
-  });
+  // Show the second modal
+  var secondModalInstance = new bootstrap.Modal(secondModal);
+  secondModalInstance.show();
 
-  // افتح المودال الأول بمجرد النقر على "join us"
-  var firstModal = new bootstrap.Modal(document.getElementById("firstModal"));
-  firstModal.show();
-});
+  // Remove the backdrop element
+  var backdrop = document.querySelector(".modal-backdrop");
+  backdrop.parentNode.removeChild(backdrop);
+}
+
+// Function to hide the second modal and show the third modal
+function hideSecondModalAndShowThirdModal() {
+  var secondModal = document.getElementById("secondModal");
+  var thirdModal = document.getElementById("thirdModal");
+
+  // Remove "show" class and add display: none inline style to hide the second modal
+  secondModal.classList.remove("show");
+  secondModal.style.display = "none";
+
+  // Show the third modal
+  var thirdModalInstance = new bootstrap.Modal(thirdModal);
+  thirdModalInstance.show();
+
+  // Remove the backdrop element
+  var backdrop = document.querySelector(".modal-backdrop");
+  backdrop.parentNode.removeChild(backdrop);
+}
+
+// Function to hide the third modal
+function hideThirdModal() {
+  var thirdModal = document.getElementById("thirdModal");
+
+  // Remove "show" class and add display: none inline style to hide the third modal
+  thirdModal.classList.remove("show");
+  thirdModal.style.display = "none";
+
+  // Remove the backdrop element
+  var backdrop = document.querySelector(".modal-backdrop");
+  backdrop.parentNode.removeChild(backdrop);
+}
 
 /////////////////readmore btn
 function toggleDescription() {
@@ -92,7 +91,7 @@ function toggleDescription() {
     button.querySelector("svg").style.transform = "rotate(180deg)";
   }
 }
-
+////////////////////////////////
 ////////////////////////////////gallery swiper
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4.2,
@@ -103,8 +102,44 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-/////////////////////////////
+///////////////////////////// modal in about
+document.addEventListener("DOMContentLoaded", function () {
+  // Get Join Us button
+  var joinUsBtn = document.getElementById("joinUsBtn");
 
+  // Add click event listener for Join Us button
+  joinUsBtn.addEventListener("click", function () {
+    // Show the first modal
+    document.getElementById("firstModal").classList.add("show");
+    document.body.classList.add("modal-open");
+    document.getElementsByClassName("modal-backdrop")[0].style.display =
+      "block";
+  });
+
+  // Get all navLinks inside first modal
+  var navLinks = document.querySelectorAll("#firstModal .navLink");
+
+  // Add click event listener for navLinks inside first modal
+  navLinks.forEach(function (navLink) {
+    navLink.addEventListener("click", function () {
+      // Hide the first modal
+      document.getElementById("firstModal").classList.remove("show");
+      document.body.classList.remove("modal-open");
+      document.getElementsByClassName("modal-backdrop")[0].style.display =
+        "none";
+
+      // Show the second modal
+      document.getElementById("secondModal").classList.add("show");
+      document.body.classList.add("modal-open");
+      document.getElementsByClassName("modal-backdrop")[0].style.display =
+        "block";
+    });
+  });
+
+  // Add click event listener for navLinks inside second modal
+  // Implement the transition to the third modal similarly
+});
+///////////////////
 $(document).ready(function () {
   $("#datepicker,#expire").datepicker({
     format: "yyyy-mm-dd",
