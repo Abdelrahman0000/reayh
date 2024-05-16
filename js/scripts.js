42b63b0ee8565d5da4a86f05b2790cbe2ab85e30
Abdelrahman0000 <khlyftbdh5@gmail.com>
2024-05-16 18:00:07 +0200

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,17 @@ $(document).ready(function () {
 
 ///////////////chane lang
 
+document
+  .querySelector(".sign-form .sign-btn") // Changed selector to target the button with the correct class
+  .addEventListener("click", function (e) {
+    if (validateSignA()) {
+      e.preventDefault(); // Prevent default form submission behavior
+      console.log(1);
+      document.getElementById("Sign").submit();
+    }
+  });
+
+//////////////////////////////
 // Select the language toggle link
 var langToggle = document.getElementById("langToggle");
 
@@ -32,50 +43,57 @@ langToggle.addEventListener("click", function (event) {
 function hideSignUpShowSecondModal() {
   var firstModal = document.getElementById("firstModal");
   var secondModal = document.getElementById("secondModal");
+  console.log(validateSignA());
+  if (validateSignA("signup-form")) {
+    // Remove "show" class and add display: none inline style to hide the first modal
+    firstModal.classList.remove("show");
+    firstModal.style.display = "none";
+    console.log("12");
+    // Show the second modal
+    var secondModalInstance = new bootstrap.Modal(secondModal);
+    secondModalInstance.show();
 
-  // Remove "show" class and add display: none inline style to hide the first modal
-  firstModal.classList.remove("show");
-  firstModal.style.display = "none";
-
-  // Show the second modal
-  var secondModalInstance = new bootstrap.Modal(secondModal);
-  secondModalInstance.show();
-
-  // Remove the backdrop element
-  var backdrop = document.querySelector(".modal-backdrop");
-  backdrop.parentNode.removeChild(backdrop);
+    // Remove the backdrop element
+    var backdrop = document.querySelector(".modal-backdrop");
+    backdrop.parentNode.removeChild(backdrop);
+  }
 }
 
 // Function to sign In the third modal
 function hideSignInModal() {
   var signinModal = document.getElementById("firstModal");
 
-  // Remove "show" class and add display: none inline style to hide the third modal
-  signinModal.classList.remove("show");
-  signinModal.style.display = "none";
+  if (validateSignA("signin-form")) {
+    // Remove "show" class and add display: none inline style to hide the third modal
+    signinModal.classList.remove("show");
+    signinModal.style.display = "none";
 
-  // Remove the backdrop element
-  var backdrop = document.querySelector(".modal-backdrop");
-  backdrop.parentNode.removeChild(backdrop);
+    // Remove the backdrop element
+    var backdrop = document.querySelector(".modal-backdrop");
+    backdrop.parentNode.removeChild(backdrop);
+  }
 }
 
 // Function to hide the second modal and show the third modal
 function hideSecondModalAndShowThirdModal() {
-  var secondModal = document.getElementById("secondModal");
-  var thirdModal = document.getElementById("thirdModal");
+  if (validateSignA("signotp-form")) {
+    var secondModal = document.getElementById("secondModal");
+    var thirdModal = document.getElementById("thirdModal");
 
-  // Remove "show" class and add display: none inline style to hide the second modal
-  secondModal.classList.remove("show");
-  secondModal.style.display = "none";
+    // Remove "show" class and add display: none inline style to hide the second modal
+    secondModal.classList.remove("show");
+    secondModal.style.display = "none";
 
-  // Show the third modal
-  var thirdModalInstance = new bootstrap.Modal(thirdModal);
-  thirdModalInstance.show();
+    // Show the third modal
+    var thirdModalInstance = new bootstrap.Modal(thirdModal);
+    thirdModalInstance.show();
 
-  // Remove the backdrop element
-  var backdrop = document.querySelector(".modal-backdrop");
-  backdrop.parentNode.removeChild(backdrop);
+    // Remove the backdrop element
+    var backdrop = document.querySelector(".modal-backdrop");
+    backdrop.parentNode.removeChild(backdrop);
+  }
 }
+// ///////////////////////////////////////////
 
 // Function to hide the third modal
 function hideThirdModal() {
@@ -116,42 +134,42 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 ///////////////////////////// modal in about
-document.addEventListener("DOMContentLoaded", function () {
-  // Get Join Us button
-  var joinUsBtn = document.getElementById("joinUsBtn");
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get Join Us button
+//   var joinUsBtn = document.getElementById("joinUsBtn");
 
-  // Add click event listener for Join Us button
-  joinUsBtn.addEventListener("click", function () {
-    // Show the first modal
-    document.getElementById("firstModal").classList.add("show");
-    document.body.classList.add("modal-open");
-    document.getElementsByClassName("modal-backdrop")[0].style.display =
-      "block";
-  });
+//   // Add click event listener for Join Us button
+//   joinUsBtn.addEventListener("click", function () {
+//     // Show the first modal
+//     document.getElementById("firstModal").classList.add("show");
+//     document.body.classList.add("modal-open");
+//     document.getElementsByClassName("modal-backdrop")[0].style.display =
+//       "block";
+//   });
 
-  // Get all navLinks inside first modal
-  var navLinks = document.querySelectorAll("#firstModal .navLink");
+//   // Get all navLinks inside first modal
+//   var navLinks = document.querySelectorAll("#firstModal .navLink");
 
-  // Add click event listener for navLinks inside first modal
-  navLinks.forEach(function (navLink) {
-    navLink.addEventListener("click", function () {
-      // Hide the first modal
-      document.getElementById("firstModal").classList.remove("show");
-      document.body.classList.remove("modal-open");
-      document.getElementsByClassName("modal-backdrop")[0].style.display =
-        "none";
+//   // Add click event listener for navLinks inside first modal
+//   navLinks.forEach(function (navLink) {
+//     navLink.addEventListener("click", function () {
+//       // Hide the first modal
+//       document.getElementById("firstModal").classList.remove("show");
+//       document.body.classList.remove("modal-open");
+//       document.getElementsByClassName("modal-backdrop")[0].style.display =
+//         "none";
 
-      // Show the second modal
-      document.getElementById("secondModal").classList.add("show");
-      document.body.classList.add("modal-open");
-      document.getElementsByClassName("modal-backdrop")[0].style.display =
-        "block";
-    });
-  });
+//       // Show the second modal
+//       document.getElementById("secondModal").classList.add("show");
+//       document.body.classList.add("modal-open");
+//       document.getElementsByClassName("modal-backdrop")[0].style.display =
+//         "block";
+//     });
+//   });
 
-  // Add click event listener for navLinks inside second modal
-  // Implement the transition to the third modal similarly
-});
+//   // Add click event listener for navLinks inside second modal
+//   // Implement the transition to the third modal similarly
+// });
 ///////////////////
 $(document).ready(function () {
   $("#datepicker,#expire").datepicker({
@@ -248,3 +266,176 @@ function showSign(contentId) {
     associatedsignType.classList.add("active");
   }
 }
+////////////////////////////////
+
+/////////////////////////////////////////////////////////sign validate
+function validateSignA(formClassName) {
+  var isValid = true;
+  // Validation for inputs
+  var inputs = document.querySelectorAll("." + formClassName + " input");
+  inputs.forEach(function (input) {
+    const errorId = "error-" + input.id;
+    if (input.value.trim() === "") {
+      isValid = false;
+      const errorDiv = document.createElement("div");
+      errorDiv.id = errorId;
+      errorDiv.className = "error";
+      errorDiv.textContent = "This field is required";
+      input.parentNode.appendChild(errorDiv);
+    } else {
+      const existingError = document.getElementById(errorId);
+      if (existingError) {
+        existingError.parentNode.removeChild(existingError);
+      }
+    }
+  });
+  console.log(isValid);
+  return isValid;
+}
+
+/////////////////////////////
+
+//////////////////////////////////////////sevices validate
+function validateContentA() {
+  var isValid = true;
+
+  // Validation for select elements
+  var selects = document.querySelectorAll(".main-form #content-a select");
+  console.log(1);
+  selects.forEach(function (select) {
+    const errorId = "error-" + select.id;
+
+    if (
+      select.value === null ||
+      select.value === undefined ||
+      select.value.trim() === "" ||
+      select.value === "نوع الخدمة" ||
+      select.value === "إحصاء عدد متلقي الخدمة" ||
+      select.value === "عمر" ||
+      select.value === "احتياجات الخدمة" ||
+      select.value === "نوع الخدمة" ||
+      select.value === " "
+    ) {
+      isValid = false;
+
+      const errorDiv = document.createElement("div");
+      errorDiv.id = errorId;
+      errorDiv.className = "error";
+      errorDiv.textContent = "This field is required";
+      select.parentNode.appendChild(errorDiv);
+    } else {
+      const existingError = document.getElementById(errorId);
+      if (existingError) {
+        existingError.parentNode.removeChild(existingError);
+      }
+    }
+  });
+
+  // Validation for inputs
+  var inputs = document.querySelectorAll(".main-form #content-a input");
+  inputs.forEach(function (input) {
+    const errorId = "error-" + input.id;
+
+    if (input.value.trim() === "") {
+      isValid = false;
+      const errorDiv = document.createElement("div");
+      errorDiv.id = errorId;
+      errorDiv.className = "error";
+      errorDiv.textContent = "This field is required";
+      input.parentNode.appendChild(errorDiv);
+    } else {
+      const existingError = document.getElementById(errorId);
+      if (existingError) {
+        existingError.parentNode.removeChild(existingError);
+      }
+    }
+  });
+
+  return isValid;
+}
+
+// Function to validate content-b
+function validateContentB() {
+  var isValid = true;
+
+  // Validation for select elements
+  var selects = document.querySelectorAll(" .main-form #content-b select");
+  selects.forEach(function (select) {
+    const errorId = "error-" + select.id;
+
+    if (
+      select.value === null ||
+      select.value === undefined ||
+      select.value.trim() === "" ||
+      select.value === "نوع الخدمة" ||
+      select.value === "إحصاء عدد متلقي الخدمة" ||
+      select.value === "عمر" ||
+      select.value === " " ||
+      select.value === "احتياجات الخدمة" ||
+      select.value === "نوع الخدمة"
+    ) {
+      isValid = false;
+
+      const errorDiv = document.createElement("div");
+      errorDiv.id = errorId;
+      errorDiv.className = "error";
+      errorDiv.textContent = "This field is required";
+      select.parentNode.appendChild(errorDiv);
+    } else {
+      const existingError = document.getElementById(errorId);
+      if (existingError) {
+        existingError.parentNode.removeChild(existingError);
+      }
+    }
+  });
+
+  // Validation for inputs
+  var inputs = document.querySelectorAll(".main-form #content-b input");
+  inputs.forEach(function (input) {
+    const errorId = "error-" + input.id;
+
+    if (input.value.trim() === "") {
+      const errorDiv = document.createElement("div");
+      errorDiv.id = errorId;
+      errorDiv.className = "error";
+      errorDiv.textContent = "This field is required";
+      input.parentNode.appendChild(errorDiv);
+    } else {
+      const existingError = document.getElementById(errorId);
+      if (existingError) {
+        existingError.parentNode.removeChild(existingError);
+      }
+    }
+  });
+
+  return isValid;
+}
+
+// Attach event listeners to buttons
+document
+  .querySelector(".main-form .next-btn")
+  .addEventListener("click", function () {
+    console.log(11);
+    if (validateContentA()) {
+      showContent("content-b");
+    }
+  });
+
+function checkValide() {
+  if (validateContentA()) {
+    showContent("content-b");
+  }
+}
+
+document
+  .querySelector(".main-form .pay-btn")
+  .addEventListener("click", function () {
+    // e.preventDefault();
+    console.log(11);
+    if (validateContentB()) {
+      // Perform payment or other actions
+      e.preventDefault();
+      console.log(1);
+      document.getElementById("form-services").submit();
+    }
+  });
